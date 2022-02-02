3661ba5f02d95609a5e2c5f6dce1087f70c4b5d1
Faisal Iqbal <iqbalf203@gmail.com>
2022-02-02 11:45:34 +0530

    Final Commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+// BMI bmiCalculator
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / (height * height);
+  res.send("Your BMI is " + bmi);
+
+});
+
+app.listen(3000, function(){
+  console.log("Server started at port no. 3000");
+});
